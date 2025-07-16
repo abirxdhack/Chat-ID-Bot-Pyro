c0db601b17fe3aa236a93b0a1201741ea147799e
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-07-16 14:06:02 +0600

    Update my.py

--- modules/my.py
+++ modules/my.py
@@ -1,3 +1,5 @@
+#Copyright @ISmartCoder 
+#Updates Channel t.me/TheSmartDev
 from pyrogram import Client, filters
 from utils import LOGGER
 from miscs.mybtn import my_buttons
@@ -11,6 +13,6 @@ async def my_command(bot: Client, message):
         "🔧 **How to Use?**\n"
         "1️⃣ Click the buttons below to share your channel or group.\n"
         "2️⃣ Receive the unique ID instantly.\n\n"
-        "> 🛠 Made with ❤️ By @ItsSmartDev",
+        "> 🛠 Made with ❤️ By @TheSmartDev",
         reply_markup=my_buttons
     )

8395ec0fea3077104c2a391d39de1dee0a0875e4
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2025-09-27 00:35:39 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#Copyright @ISmartCoder 
-#Updates Channel t.me/TheSmartDev
 from bot import bot
 from utils import LOGGER
 from core.start import start
@@ -7,6 +5,7 @@ from modules.help import help_command
 from modules.my import my_command
 from modules.admin import admin_command
 from modules.me import me_command
+from modules.info import info_command
 from modules.fwd import handle_forwarded_message
 from modules.username import username_command
 from shared.chatinfo import handle_message
